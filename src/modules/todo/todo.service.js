994fc65360b221ef084a0092fc093f26b69f339d
TheSiteshKumar <thesiteshkumar@gmail.com>
2025-11-23 15:30:17 +0530

    upload issues detected

--- src/modules/todo/todo.service.js
+++ src/modules/todo/todo.service.js
@@ -11,8 +11,17 @@ export const createTodo = asyncHandler(async (req, res, next) => {
     let image = null;
     let files = [];
 
+
+    //     if(req?.image){
+    //   const file=req.files.rfpDocument;
+    //   const response= await uploadDocumentOnCloudinary(file,"tenders");
+    //   value.rfpDocument={
+    //       url:response.secure_url,
+    //       public_id:response.public_id
+    //   }
+    // }
     // Handle Image Upload
-    if (req.files && req.files.image && req.files.image.length > 0) {
+    if (req?.files?.image ) {
         const imgResponse = await uploadOnCloudinary(req.files.image[0].path);
         if (imgResponse) {
             image = {
